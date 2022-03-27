25c82e80e9c6b43136d47e0d6fb510b249c90dc7
Divyateja Pasupuleti <divyateja2004@gmail.com>
2022-03-27 21:56:38 +0530

    navbar fix

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,19 +2,27 @@ import { Link } from "react-router-dom"
 
 export default function Header() {
     return (
-        <div className="header">
-            <div className="quickkiss">
+        // <div className="header">
+        //     <div className="quickkiss">
+        //     <Link className="header-title" to="/">Verba Maximus</Link>
+        //     </div>
+        //     <div className="header-links">
+                
+        //     </div>
+        // </div>
+        <nav class="db dt-l w-100 border-box pa3 ph5-l bg-black">
+        <div className="quickkiss">
             <Link className="header-title" to="/">Verba Maximus</Link>
-            </div>
-            <div className="header-links">
-                <Link className="h-link" to="/">Home</Link>
-                <Link className="h-link" to="/Events">Events</Link>
-                <Link className="h-link" to="/Team">Team</Link>
-                <Link className="h-link" to="/Sponsors">Sponsors</Link>
-                <Link className="h-link" to="/Contact">Contact</Link>
-                <Link className="h-link" to="/Schedule">Schedule</Link>
-                <Link className="h-reg-link bg-blue br3 pa2 nl2 nt2 nb2" to="/Register">Register</Link>
-            </div>
         </div>
+        <div class="db dtc-l v-mid w-100 w-75-l tc tr-l">
+            <Link className="white link dim dark-gray f6 f5-l dib mr3 mr4-l" to="/">Home</Link>
+            <Link className="white link dim dark-gray f6 f5-l dib mr3 mr4-l" to="/Events">Events</Link>
+            <Link className="white link dim dark-gray f6 f5-l dib mr3 mr4-l" to="/Team">Team</Link>
+            <Link className="white link dim dark-gray f6 f5-l dib mr3 mr4-l" to="/Sponsors">Sponsors</Link>
+            <Link className="white link dim dark-gray f6 f5-l dib mr3 mr4-l" to="/Contact">Contact</Link>
+            <Link className="white link dim dark-gray f6 f5-l dib mr3 mr4-l" to="/Schedule">Schedule</Link>
+            <Link className="white link dim bg-blue f6 f5-l dib mr3 mr4-l pa2 br1" to="/Register">Register</Link>
+        </div>
+        </nav>
     )
   }

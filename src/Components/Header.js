4308f23e35c7691524ade0fe3213c9e3259c4bad
Divyateja Pasupuleti <divyateja2004@gmail.com>
2022-03-27 22:08:11 +0530

    navbar fix

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -15,12 +15,12 @@ export default function Header() {
             <Link className="header-title" to="/">Verba Maximus</Link>
         </div>
         <div className="db dtc-l v-mid w-100 w-75-l tc tr-l">
-            <Link className="white link dim dark-gray f6 f5-l dib mr3 mr4-l pa2" to="/">Home</Link>
-            <Link className="white link dim dark-gray f6 f5-l dib mr3 mr4-l pa2" to="/Events">Events</Link>
-            <Link className="white link dim dark-gray f6 f5-l dib mr3 mr4-l pa2" to="/Team">Team</Link>
-            <Link className="white link dim dark-gray f6 f5-l dib mr3 mr4-l pa2" to="/Sponsors">Sponsors</Link>
-            <Link className="white link dim dark-gray f6 f5-l dib mr3 mr4-l pa2" to="/Contact">Contact</Link>
-            <Link className="white link dim dark-gray f6 f5-l dib mr3 mr4-l pa2" to="/Schedule">Schedule</Link>
+            <Link className="white link dim dark-gray f6 f5-l dib mr3 mr4-l" to="/">Home</Link>
+            <Link className="white link dim dark-gray f6 f5-l dib mr3 mr4-l" to="/Events">Events</Link>
+            <Link className="white link dim dark-gray f6 f5-l dib mr3 mr4-l" to="/Team">Team</Link>
+            <Link className="white link dim dark-gray f6 f5-l dib mr3 mr4-l" to="/Sponsors">Sponsors</Link>
+            <Link className="white link dim dark-gray f6 f5-l dib mr3 mr4-l" to="/Contact">Contact</Link>
+            <Link className="white link dim dark-gray f6 f5-l dib mr3 mr4-l" to="/Schedule">Schedule</Link>
             <Link className="white link dim bg-blue f6 f5-l dib mr3 mr4-l pa2 br1" to="/Register">Register</Link>
         </div>
         </nav>
